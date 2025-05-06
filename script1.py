c8793e05d3a7de2f5f3c4a1ebe52f1403e6badc8
salise-george <23101226@usc.edu.ph>
2025-05-06 16:55:00 +0800

    hahaahahha

--- script1.py
+++ script1.py
@@ -1,24 +1,31 @@
 from netmiko import ConnectHandler
 
 device = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.1.1',
-    'username': 'admin',
-    'password': 'your_password',
+    	'device_type': 'cisco_ios',
+    	'host': '192.168.10.1',
+	'username': 'admin',
+	'password': 'cisco',
+	'secret': 'cisco',
 }
 
 # Establish SSH connection
 net_connect = ConnectHandler(**device)
 
-# Send command
-output = net_connect.send_command('show ip int brief')
-print(output)
+net_connect.enable()
 
-# Send configuration
-config_commands = [
-    'interface loopback0',
-    'ip address 10.10.10.1 255.255.255.0',
-]
-net_connect.send_config_set(config_commands)
+numofSUB = int(input("How many subints do you want to make?: "))
+
+
+for x in range(1, numofSUB):
+	sub_int = x * 10
+	output = net_connect.send_config_set(
+		[
+		f"int f0/0.{sub_int}",
+		f"encapsulation dot1Q {sub_int}",
+		f"ip add 192.168.{sub_int}.1 255.255.255.0",
+		"exit",
+		]
+	)
+	print(output)
 
 net_connect.disconnect()
